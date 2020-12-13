49068603cd1bcd8df7fbd2ae3f3bb10dfed0e6b2
tubasthebest <tubasthebest@gmail.com>
2020-12-13 12:08:50 -0700

    load city data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector('#city');
 var pastCityBtnEl = document.querySelector('#previous-cities')
+var cityTitleEl = document.querySelector('#current-city');
+var weatherDisplayEl = document.querySelector('#weather-container');
 var cities = [];
+var apiKey = "4e58e97fdd4f6e5374486a7e4a85fd81";
 
 
 var searchedCity = function(event){
@@ -11,6 +14,7 @@ var searchedCity = function(event){
 
     if (currentCity) {
         cities.push(currentCity);
+        getCurrentWeather(currentCity);
         cityInputEl.value = "";
         saveCity();
     }
@@ -28,10 +32,10 @@ var loadPrevious = function() {
     for (i = 0; i < cities.length; i++) {
         previousSearch(cities[i]);
     }
-}
+};
 
 var previousSearch = function(pastCity) {
-    console.log(pastCity);
+    //console.log(pastCity);
     pastCityEl = document.createElement("button");
     pastCityEl.textContent = pastCity;
     pastCityEl.classList = "d-flex w-100 btn-light barder p-2";
@@ -39,7 +43,24 @@ var previousSearch = function(pastCity) {
     pastCityEl.setAttribute("type", "submit");
 
     pastCityBtnEl.prepend(pastCityEl);
-}
+};
+
+var getCurrentWeather = function(city) {
+    var apiSite = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiSite)
+    .then(function(response){
+        response.json().then(function(data){
+            loadWeather(data, city);
+        });
+    });
+};
+
+var loadWeather = function(weather, currentCity) {
+    weatherDisplayEl.textContent = "";
+    cityTitleEl.textContent = currentCity;
+    console.log(weather);
+};
 
 
 userFormEl.addEventListener("submit", searchedCity);
