ae3bd87605595a948376f8518535ea11184d2839
tubasthebest <tubasthebest@gmail.com>
2020-12-13 18:31:49 -0700

    mobile responsive and finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,11 @@ var weatherDisplayEl = document.querySelector('#weather-container');
 var forecastDisplayEl = document.querySelector('#forecast');
 var forecastTitleEl = document.querySelector('#forecast-title');
 var previousCityEl = document.querySelector('#previous-cities');
+var currentWeatherEl = document.querySelector('#current-weather');
+var fiveDayEl = document.querySelector('#five-day');
 var cities = [];
 var apiKey = "4e58e97fdd4f6e5374486a7e4a85fd81";
+document.querySelector('#city').style.height="35px";
 
 
 var searchedCity = function(event){
@@ -18,7 +21,6 @@ var searchedCity = function(event){
     if (currentCity) {
         getCurrentWeather(currentCity);
         cityInputEl.value = "";
-        saveCity(currentCity);
     }
     else {
         alert("Please enter a city before searching");
@@ -46,7 +48,7 @@ var previousSearch = function(pastCity) {
     //console.log(pastCity);
     pastCityEl = document.createElement("button");
     pastCityEl.textContent = pastCity;
-    pastCityEl.classList = "d-flex w-100 btn-light barder p-2";
+    pastCityEl.classList = "d-flex w-100 btn-light border p-2";
     pastCityEl.setAttribute("data-city", pastCity);
     pastCityEl.setAttribute("type", "submit");
 
@@ -58,15 +60,23 @@ var getCurrentWeather = function(city) {
 
     fetch(apiSite)
     .then(function(response){
-        response.json().then(function(data){
-            loadWeather(data, city);
-        });
+        if (response.ok){
+            response.json().then(function (data) {
+                loadWeather(data, city);
+                saveCity(city);
+            });  
+        }
+        else {
+            alert("Error: " + response.statusText);
+            return;
+        }
     });
 };
 
 var loadWeather = function(weather, currentCity) {
     weatherDisplayEl.textContent = "";
     cityTitleEl.textContent = currentCity;
+    currentWeatherEl.classList.add('border', 'p-2');
     //console.log(weather);
 
     var theDate = document.createElement('span');
@@ -145,12 +155,13 @@ var getForecast = function(lat, lon) {
 var loadForecast = function(weather){
     forecastTitleEl.textContent = "5-Day Forecast:"
     forecastDisplayEl.textContent = "";
+    fiveDayEl.classList.add('border', 'p-2');
 
     for (i=1; i < 6; i++) {
         var dayWeather = (weather.daily[i]);
 
         var weatherForecastEl = document.createElement('div');
-        weatherForecastEl.classList = "card bg-primary text-light m-2"
+        weatherForecastEl.classList = "card bg-primary text-light m-2 col-sm"
 
         var dayDate = document.createElement('h5');
         dayDate.textContent = moment.unix(dayWeather.dt).format('M/DD/YYYY');
